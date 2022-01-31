73fb173ca9245387260a2477c5cb0972acbc367d
LeozittoH <davidleonam10@gmail.com>
2022-01-31 14:22:44 -0300

    atualizando repositorio

---
+++ Digital Innovation/MRV Bootcamp/Introdução ao JavaScript/DIO/assets/js/script.js
@@ -0,0 +1,19 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+function increment () {
+    currentNumber = currentNumber + 1; 
+    if (currentNumber >= 10) {
+        currentNumber = 10;
+    }
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement () {
+    currentNumber = currentNumber - 1; 
+    if (currentNumber <= 0) {
+        currentNumber = 0;
+    }
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
